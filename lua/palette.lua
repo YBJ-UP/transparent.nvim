8d0b85ecff605f4ad6964b60ac6699548a3ab48d
Yael <243678@ids.upchiapas.edu.mx>
2026-06-17 20:10:56 -0600

    chore: initial commit

---
+++ lua/palette.lua
@@ -0,0 +1,18 @@
+local palette = {
+	bg = "none",
+	fg = "#dddddd",
+	fg_unfocused = "#aaaaaa",
+	primary_text = "#ffffff",
+	functions = "#80D5D3",
+	keywords = "#4DDAD9",
+	comments = "#999999",
+	constants = "#80D5D3",
+	strings = "#0ef3ff",
+	special = "#63DBB5",
+	visual = "#FFB59F",
+	search = "#CFBCFF",
+	substitute = "#5C7FD6",
+	error = "#dd5555"
+}
+
+return palette
---
+++ lua/scheme.lua
@@ -0,0 +1,49 @@
+local colors = require("lua.palette")
+local G = {}
+
+G.setup =  function()
+	print("lol")
+	vim.cmd("highlight clear")
+	vim.cmd("syntax reset")
+
+	vim.o.background = "dark"
+	vim.g.colors_name = "transparent"
+	local set = vim.api.nvim_set_hl
+
+	set(0, "Normal", { bg = colors.bg, fg = colors.fg })
+	set(0, "NormalNc", { bg = colors.bg, fg = colors.fg_unfocused })
+
+	set(0, "CursorColumn", { bg = colors.bg })
+
+	set(0, "CursorLine", { bg = colors.bg })
+	set(0, "Visual", { bg = colors.bg, fg = colors.visual })
+
+	set(0, "Comment", { fg = colors.comments, italic = true })
+	set(0, "Constant", { fg = colors.constants })
+	set(0, "@Variable", { fg = colors.special })
+	set(0, "String", { fg = colors.strings })
+	set(0, "Identifier", { fg = colors.fg })
+	set(0, "Statement", { fg = colors.keywords })
+	set(0, "Type", { fg = colors.keywords, italic = true })
+	set(0, "Operator", { fg = colors.constants })
+	set(0, "Macro", { fg = colors.fg })
+	set(0, "Delimiter", { fg = colors.special })
+
+	set(0, "Substitute", { bg = colors.bg, fg = colors.substitute })
+	set(0, "IncSearch", { bg = colors.bg, fg = colors.search })
+	set(0, "CurSearch", { bg = colors.bg, fg = colors.search })
+	set(0, "Search", { bg = colors.bg , fg = colors.search })
+
+	set(0, "Special", { fg = colors.constants })
+
+	set(0, "PreProc", { fg = colors.special })
+
+	set(0, "Keyword", { fg = colors.keywords })
+
+	set(0, "Function", { fg = colors.functions })
+	set(0, "TSFunction", { fg = colors.fg })
+
+	set(0, "Error", { fg = colors.error, bold = true })
+end
+
+return G
