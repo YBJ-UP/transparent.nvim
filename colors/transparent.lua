53b10604da91acc570159bdb617e38b759aad5b0
Yae <243678@ids.upchiapas.edu.mx>
2026-07-08 20:25:54 -0600

    fix: change default theme back to blue
    my dum ahh forgot to push that

--- colors/transparent.lua
+++ colors/transparent.lua
@@ -1,2 +1,2 @@
 local palettes = require("transparent_palette")
-require("transparent_scheme").set_transparent_scheme(palettes.reddish)
+require("transparent_scheme").set_transparent_scheme(palettes.blueish)
